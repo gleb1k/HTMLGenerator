5d858c6c50e78fc6a866cf3a7f7a75451d6f76e1
Гафеев Глеб <gleb.gafeev@mail.ru>
2022-11-14 17:15:32 +0300

    Add project files.

---
+++ HTMLGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using HTMLGeneratorLibrary;
+
+string result = "Hello World";
+
+string HtmlPage = "<html>\r\n<head>\r\n<title>Шапка</title>\r\n</head>\r\n<body>\r\n{{result}}\r\n</body>\r\n</html>";
+
+var gs = new GeneratorService();
+var res = gs.GetHTML(HtmlPage, result);
+
+var n = 0; 
---
+++ HTMLGeneratorLibrary/GeneratorService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLGeneratorLibrary
+{
+    public class GeneratorService : IHtmlGeneratorService
+    {
+        private const string customTag = "{{";
+        public string GetHTML(string template, object model)
+        {
+            //Пришли нулы
+            if (string.IsNullOrEmpty(template))
+                return "";
+            if (!template.Contains($"{customTag}"))
+                return ""; 
+            if (model == null)
+                return template.Replace("{{result}}", null);
+            return template.Replace("{{result}}",model.ToString());
+        }
+
+        public string GetHTML(Stream stream, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetHTML(byte[] bytes, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetHTMLToByte(string template, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetHTMLToByte(Stream stream, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetHTMLToByte(byte[] bytes, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream GetHTMLToStream(string template, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream GetHTMLToStream(Stream stream, object model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream GetHTMLToStream(byte[] bytes, object model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
